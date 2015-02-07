1da964868b7f27da18872a4665d97a5d44543eb9
Lee Miller <lee@blackandred.io>
2015-02-07 15:45:04 -0700

    Updated semitone model to not post

--- app/scripts/models/semitone.js
+++ app/scripts/models/semitone.js
@@ -2,5 +2,9 @@ module.exports = Backbone.Model.extend({
     defaults: {
         note: undefined,
         color: undefined
+    },
+
+    sync: function() {
+        // no need to save, no need for sync!
     }
 });

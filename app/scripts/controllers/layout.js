e5579dc434c5f041bb16a48fc3545340f5c8900f
Lee Miller <lee@blackandred.io>
2015-02-08 00:11:45 -0700

    Updated layout to show constellation, currently major scale

--- app/scripts/controllers/layout.js
+++ app/scripts/controllers/layout.js
@@ -2,6 +2,7 @@ var LayoutView = require('../views/layout');
 var ColorWheelView = require('scripts/views/color-wheel');
 var TonicSelectorView = require('../views/tonic-selector');
 var Semitones = require('scripts/collections/semitones');
+var ConstellationView = require('../views/constellation');
 var config = require('config');
 
 var allChromaticScales = {};
@@ -9,59 +10,79 @@ _.each(config.allPitches.split(' '), function(tonic) {
     allChromaticScales[tonic] = new Semitones(tonic);
 });
 
-var stage;
-var wheel;
-
 module.exports = Backbone.Marionette.Controller.extend({
+    baseLayer: null,
+    mouseoverLayer: null,
+    stage: null,
+    wheel: null,
+
     initialize: function(options) {
-        console.log(options);
         this.options = options || {};
     },
 
     show: function() {
         var layoutView = new LayoutView();
         this.options.region.show(layoutView);
+        this._initializeStageAndLayers();
+
         var tonicSelectorView = new TonicSelectorView();
         tonicSelectorView.on('before:destroy', function() {
             tonicSelectorView.off();
         }, this);
-        tonicSelectorView.on('change:tonic', function(newTonic) {
-            this._clearPreviousWheel();
-            this._showColorWheel(newTonic);
-        }, this);
+        tonicSelectorView.on('change:tonic', this._changeTonic, this);
         layoutView.getRegion('tonicSelector').show(tonicSelectorView);
         this._showColorWheel(config.defaultTonic);
+        this._showConstellation();
+    },
+
+    _changeTonic: function(newTonic) {
+        this._clearPreviousWheel();
+        this._initializeStageAndLayers();
+        this._showColorWheel(newTonic);
+        this._showConstellation();
+    },
+
+    _initializeStageAndLayers: function() {
+        this.stage = new Konva.Stage({
+            container: $('#color-wheel-container')[0],
+            width: 500,
+            height: 500
+        });
+
+        this.baseLayer = new Konva.Layer();
+        this.mouseoverLayer = new Konva.Layer();
+        this.stage.add(this.baseLayer, this.mouseoverLayer);
     },
 
     _clearPreviousWheel: function() {
-        if (_.isUndefined(stage)) return;
-        wheel.destroy();
-        stage.clear();
-        stage.destroy();
-        stage = undefined;
-
+        if (_.isNull(this.wheel)) return;
+        this.wheel.off();
+        this.wheel.destroy();
+        this.baseLayer.destroy();
+        this.mouseoverLayer.destroy();
+        this.stage.clear();
+        this.stage.destroy();
+        this.stage = null;
     },
 
     _showColorWheel: function(tonic) {
-        _.defer(function() {
-            if (_.isUndefined(stage)) {
-                stage = new Konva.Stage({
-                    container: $('#color-wheel-container')[0],
-                    width: 500,
-                    height: 500
-                });
-            }
-            
-            var baseLayer = new Konva.Layer();
-            var mouseoverLayer = new Konva.Layer();
-            stage.add(baseLayer, mouseoverLayer);
+        console.log(tonic);
+        this.wheel = new ColorWheelView({
+            stage: this.stage,
+            baseLayer: this.baseLayer,
+            mouseoverLayer: this.mouseoverLayer, 
+            semitones: allChromaticScales[tonic]
+        });
+        this.wheel.on('change:tonic', this._changeTonic, this);
+    },
 
-            wheel = new ColorWheelView({
-                stage: stage,
-                baseLayer:baseLayer,
-                mouseoverLayer: mouseoverLayer, 
-                semitones: allChromaticScales[tonic]
-            });
+    _showConstellation: function() {
+        var constellationLayer = new Konva.Layer();
+        this.stage.add(constellationLayer);
+        var constellationView = new ConstellationView({
+            stage: this.stage,
+            layer: constellationLayer,
+            pitches: [0, 2, 4, 5, 7, 9, 11]
         });
     }
 })

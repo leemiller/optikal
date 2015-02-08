ba2de7a2cd421cc395d01c222ee355ac4dff2b73
Lee Miller <lee@blackandred.io>
2015-02-08 11:44:48 -0700

    Added modes to config

--- app/config.js
+++ app/config.js
@@ -1,4 +1,14 @@
 module.exports = {
     defaultTonic: 'C',
-    allPitches: 'A A# B C C# D D# E F F# G G#'  
+    allPitches: 'A A# B C C# D D# E F F# G G#',
+    defaultMode: 'Ionian',
+    allModes: {
+        Ionian: [0, 2, 4, 5, 7, 9, 11],
+        Dorian: [0, 2, 3, 5, 7, 9, 10],
+        Phrygian: [0, 1, 3, 5, 7, 8, 10],
+        Lydian: [0, 2, 4, 6, 7, 9, 11],
+        Mixolydian: [0, 2, 4, 5, 7, 9, 10],
+        Aeolian: [0, 2, 3, 5, 7, 8, 10],
+        Locrian: [0, 1, 3, 5, 6, 8, 10]
+    }  
 };

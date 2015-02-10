bc90b011fd0fbffad4283e7d0e247a826ab58163
Lee Miller <lee@blackandred.io>
2015-02-10 14:29:30 -0700

    Added chromatic scales to config, moved default items to own module

--- app/config.js
+++ app/config.js
@@ -1,8 +1,14 @@
+var Notes = require('scripts/collections/notes');
+var notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+var allChromaticScales = {};
+_.each(notes, function(tonic) {
+    allChromaticScales[tonic] = new Notes(tonic);
+});
+
 module.exports = {
-    defaultTonic: 'C',
-    allPitches: ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'],
-    defaultMode: 'Ionian',
-    allModes: {
+    notes: notes,
+    chromaticScales: allChromaticScales,
+    modes: {
         Ionian: [0, 2, 4, 5, 7, 9, 11],
         Dorian: [0, 2, 3, 5, 7, 9, 10],
         Phrygian: [0, 1, 3, 5, 7, 8, 10],
@@ -12,5 +18,5 @@ module.exports = {
         Locrian: [0, 1, 3, 5, 6, 8, 10],
         'Iwato Scale': [0, 1, 10, 5, 6],
         'Major Pentatonic Scale': [0, 9, 2, 4, 7]
-    }  
+    }
 };
---
+++ app/defaults.js
@@ -0,0 +1,10 @@
+module.exports = {
+    tonic: 'C',
+    mode: 'Ionian',
+    instruments: {
+        'fretted-string': {
+            frets: 12,
+            strings: ['E', 'B', 'G', 'D', 'A', 'E']
+        }
+    }
+};
